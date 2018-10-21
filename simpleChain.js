da182a660fd5334be9b46633ae1fc0ea400a4e96
Tiago Guimarães <tilacog@gmail.com>
2018-10-21 16:35:52 -0300

    fix typo

--- simpleChain.js
+++ simpleChain.js
@@ -3,7 +3,7 @@
    |  =========================================================*/
 
 const SHA256 = require('crypto-js/sha256');
-const {db, addLevelDBData} ,= require('./levelSandbox');
+const {db, addLevelDBData} = require('./levelSandbox');
 
 
 /* ===== Block Class ==============================
