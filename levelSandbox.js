29eba548fbd85f4a91870931cace3b7cc73579d1
Tiago Guimarães <tilacog@gmail.com>
2018-10-21 16:15:01 -0300

    suppress unknown block execution

--- levelSandbox.js
+++ levelSandbox.js
@@ -44,11 +44,9 @@ function addDataToLevelDB(value) {
    |    Bitcoin blockchain adds 8640 blocks per day                               |
    |     ( new block every 10 minutes )                                           |
    |  ===========================================================================*/
-
-
-(function theLoop (i) {
-    setTimeout(function () {
-        addDataToLevelDB('Testing data');
-        if (--i) theLoop(i);
-    }, 100);
-})(10);
+// (function theLoop (i) {
+//     setTimeout(function () {
+//         addDataToLevelDB('Testing data');
+//         if (--i) theLoop(i);
+//     }, 100);
+// })(10);
